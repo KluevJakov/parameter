0ac8e2ee3b6e35871e3486af74d0e3661a102f80
KluevJakov <kluevja@gmail.com>
2023-07-01 03:01:26 +0400

    init

---
+++ src/main/java/ru/kluevja/econ/EconApplication.java
@@ -0,0 +1,13 @@
+package ru.kluevja.econ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconApplication.class, args);
+	}
+
+}
